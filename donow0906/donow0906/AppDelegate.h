b02e1525bdd85eb13b2faceeb04e094778b268c6
進藤 <paionia21ns@gmail.com>
2013-09-06 19:34:14 +0900

    //初期ファイル

---
+++ donow0906/donow0906/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  donow0906
+//
+//  Created by techcamp on 13/09/06.
+//  Copyright (c) 2013年 techcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ donow0906/donow0906/login.m
@@ -0,0 +1,38 @@
+//
+//  login.m
+//  donow0906
+//
+//  Created by techcamp on 13/09/06.
+//  Copyright (c) 2013年 techcamp. All rights reserved.
+//
+
+#import "login.h"
+
+@interface login ()
+
+@end
+
+@implementation login
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
