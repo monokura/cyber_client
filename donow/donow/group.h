8be498ddc22a6f5e3feb52a758373e527e83ed70
進藤 <paionia21ns@gmail.com>
2013-09-07 13:48:29 +0900

    //finish mypage_group

---
+++ donow/donow/group.h
@@ -0,0 +1,13 @@
+//
+//  group.h
+//  donow
+//
+//  Created by techcamp on 13/09/07.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface group : UITableViewController
+
+@end
---
+++ donow/donow/mypage.h
@@ -0,0 +1,13 @@
+//
+//  mypage.h
+//  donow
+//
+//  Created by techcamp on 13/09/07.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mypage : UITableViewController
+
+@end
