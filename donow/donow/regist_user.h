98214fc75615450c462d88f9e8dd69c6c6ab06d4
進藤 <paionia21ns@gmail.com>
2013-09-10 14:39:27 +0900

    //make regist_user

---
+++ donow/donow/regist_user.h
@@ -0,0 +1,13 @@
+//
+//  regist_user.h
+//  donow
+//
+//  Created by techcamp on 13/09/10.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface regist_user : UIViewController
+
+@end
---
+++ donow/donow/regist_user.m
@@ -0,0 +1,38 @@
+//
+//  regist_user.m
+//  donow
+//
+//  Created by techcamp on 13/09/10.
+//
+//
+
+#import "regist_user.h"
+
+@interface regist_user ()
+
+@end
+
+@implementation regist_user
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
