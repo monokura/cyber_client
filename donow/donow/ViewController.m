0d87d59a59fa4fe6bb564aff48e7a198565e8756
進藤 <paionia21ns@gmail.com>
2013-09-12 21:58:49 +0900

    //CAかえる

--- donow/donow/ViewController.m
+++ donow/donow/ViewController.m
@@ -26,15 +26,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    UIImageView *backgroundImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"blackboard.png"]];
-    backgroundImage.frame = CGRectMake(0, 0, 480, 480);
-    [self.view addSubview:backgroundImage];
 
     UIImage *check_off = [UIImage imageNamed:@"check_off.png"];     //ボタン画像生成
     UIImage *check_on = [UIImage imageNamed:@"check_on.png"];       //ボタン画像生成
     
     //rootview///////////////////////////////////
-    _rvc.view.frame = CGRectMake(0, 0, 480, 240);                   //rootviewサイズ
+    _rvc.view.frame = CGRectMake(0, 0, 480, 480);                   //rootviewサイズ
     _rvc.view.backgroundColor = [UIColor whiteColor];               //rootview背景
     _rvc = [[RootViewController alloc] init];
     [self.view addSubview:_rvc.view];                               //rootviewをviewに追加
@@ -42,7 +39,7 @@
 
     //setting_button/////////////////////////////
     _setting =  [UIButton buttonWithType:UIButtonTypeCustom];
-    _setting.frame = CGRectMake(430, 250, 50, 50);                  //ボタン位置設定
+    _setting.frame = CGRectMake(400, 200, 50, 50);                  //ボタン位置設定
     UIImage *img = [UIImage imageNamed:@"check_off.png"];           //ボタン画像生成
     [_setting setBackgroundImage:img forState:UIControlStateNormal];//画像セット
     [_setting addTarget:self
@@ -53,7 +50,7 @@
     
     //left_button/////////////////////////////
     _left_button =  [UIButton buttonWithType:UIButtonTypeCustom];
-    _left_button.frame = CGRectMake(90, 180, 50, 50);               //ボタン位置設定
+    _left_button.frame = CGRectMake(200, 180, 50, 50);               //ボタン位置設定
     [_left_button setBackgroundImage:check_off
                             forState:UIControlStateNormal];         //画像セット
     [_left_button sizeToFit];                                       //サイズ自動調整関数
@@ -80,7 +77,10 @@
     
     //flashcardname/////////////////////////////
     _flashcardname =  [[UILabel alloc] init];
-    _flashcardname.frame = CGRectMake(210, 30, 200, 50);               //ボタン位置設定
+    _flashcardname.frame = CGRectMake(210, 30, 200, 50);
+    [_flashcardname setBackgroundColor:[UIColor clearColor]];
+    //_flashcardname.alpha = 0.2;
+    _flashcardname.textAlignment = NSTextAlignmentCenter;
     _flashcardname.contentMode=UIViewContentModeCenter;
     _flashcardname.text = @"単語帳";
     [self.view addSubview:_flashcardname];                            // settingをviewに追加
@@ -99,7 +99,12 @@
        forControlEvents:UIControlEventTouchUpInside];               // ボタンタップ時呼び出しメソッド(button_Tapped)
     [self.view addSubview:_back];                                // settingをviewに追加
     ///////////////////////////////////////////////
-    
+ 
+    UIImageView *backgroundImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"blackboard.png"]];
+    backgroundImage.frame = CGRectMake(0, 0, 480, 480);
+    [self.view addSubview:backgroundImage];
+    [self.view sendSubviewToBack:backgroundImage];
+
 }
 
 - (void)didReceiveMemoryWarning
